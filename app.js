55d8b6e4352ef3e23eadc09423e3f5da50a76dbe
Golden <goldenm4@BKSQ-Q2E7-MAC.hsd1.pa.comcast.net>
2019-06-12 23:31:31 -0400

    loop through and present blog posts on home route

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent });
-  console.log(posts);
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts
+  });
 });
 
 app.get("/about", function (req, res) {
